53345a165b6ae75589e3d00740e5ef3d603eb10b
Jeremy Albright <myrlin1@gmail.com>
2019-11-22 08:34:48 -1000

    Rename to reflect purpose more clearly

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,4 @@ export { isPrintable, ansiPattern, splitVTString } from './ansi';
 export { lineCount, lastRowCol } from './lines';
 export { getEmptyState } from './state';
 export { getKeysList, getPrintCharList, keyMatches } from './keys';
-export { BypassStream } from './readable';
+export { BypassStream } from './bypass';
